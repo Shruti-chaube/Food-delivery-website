fd6160c3e1cdbfd191f5a38648d00986b602d3a1
shruti <shrutichaube27@gmail.com>
2021-06-21 13:25:54 +0530

    git

--- second.php
+++ second.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feedback</title>
+    <link rel ="stylesheet"href="style/second.css">
 </head>
 <body>
 <?php
